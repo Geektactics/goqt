80e0e5f66e4a8738003e8e270ba42a28dda2d449
BuildTools <unconfigured@null.spigotmc.org>
2015-10-29 00:22:02 -0700

    I have no idea

---
+++ qt.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+import "math/rand"
+
+func main() {
+    fmt.Println("This is Qt. Trust me. I would not lie to you. Good day.")
+    if rand.New(rand.NewSource(99)).Uint32() == 42 {
+        fmt.Println("I like apple pie.")
+    }
+}
