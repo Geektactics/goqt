5d195a1163cf71f0128ec2731ded2831b48d1325
Josh Max <joshumax@hotmail.com>
2016-10-31 21:38:42 -0700

    Update qt.go

--- qt.go
+++ qt.go
@@ -5,6 +5,6 @@ import "math/rand"
 func main() {
     fmt.Println("This is Qt. Trust me. I would not lie to you. Good day.")
     if rand.New(rand.NewSource(99)).Uint32() == 42 {
-        fmt.Println("I like apple pie.")
+        fmt.Println("I like delicious apple pie.")
     }
 }
